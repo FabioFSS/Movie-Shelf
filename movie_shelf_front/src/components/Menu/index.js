5354172d55e3fa7008547cf711f22e86c050e830
Welvis Souza <welvisconta@gmaile.com>
2022-04-27 22:29:53 -0300

    create component menu

---
+++ movie_shelf_front/src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './styles.scss'
+import logo from "../../assets/logo.png";
+import profile from "../../assets/profile.png";
+
+export default function Menu() {
+    return (
+        <div className='navbar'>
+            <div style={{ display: "flex", width: "600px" }}>
+                <h1 className='menu'>Tv</h1>
+                <h1 className='menu'>Series</h1>
+                <h1 className='menu'>Movies</h1>
+            </div>
+            <img className='logo' src={logo} />
+            <div style={{ display: "flex", alignItems: "center", width: "600px" }}>
+                <input type="text" name="search" className='serch' placeholder='Search' />
+                <img className='profile' src={profile} onclick="" />
+            </div>
+        </div>
+    );
+}
--- movie_shelf_front/src/pages/Details/index.js
+++ movie_shelf_front/src/pages/Details/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './styles.scss'
-import logo from "../../assets/logo.png";
-import profile from "../../assets/profile.png";
+import Menu from '../../components/Menu/index.js';
 
 export default function Scroll() {
 
@@ -33,19 +32,8 @@ export default function Scroll() {
     return (
         <div className='wrapper'>
             <div className='backgrundImg' style={{ backgroundImage: `url(${data.uriBackground})` }} />
-            <div className='navbar'>
-                <div style={{ display: "flex", width: "600px" }}>
-                    <h1 className='menu'>Tv</h1>
-                    <h1 className='menu'>Series</h1>
-                    <h1 className='menu'>Movies</h1>
-                </div>
-                <img className='logo' src={logo} />
-                <div style={{ display: "flex", alignItems: "center", width: "600px" }}>
-                    <input type="text" name="search" className='serch' placeholder='Search' />
-                    <img className='profile' src={profile} onclick="" />
-                </div>
-            </div>
-            <div className='teste1'>
+            <Menu/>
+            <div className='container'>
                 <div className='containerPoster'>
                     <img className='poster' src={uri} />
                     <p className='vote'>{data.vote_average}</p>
@@ -55,7 +43,7 @@ export default function Scroll() {
                     <p className='overview'>{data.overview}</p>
                     <div className='casts'>
                         {data.actors.map((actor) =>
-                            <div className='teste'>
+                            <div className='containerInfoCasts'>
                                 <img className='imgActors' src={actor} />
                                 <p className='nameActor'>Name Actor</p>
                             </div>
