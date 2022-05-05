26f51502f327832d5b2df4dce13d7b37ae892cc9
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-05-05 19:02:42 -0300

    added profile option to profile page

--- movie_shelf_front/src/components/scripts/ProfileMenu.js
+++ movie_shelf_front/src/components/scripts/ProfileMenu.js
@@ -4,6 +4,7 @@ function ProfileMenu(){
     return (
     <div className="profile_menu">
         <ul className="profile_menu_options">
+            <li><a href="/profile">Profile</a></li>
             <li><a href="/lists">My lists</a></li>
             <li><a href="/progress">Progress</a></li>
             <li><a href="/ratings">Ratings</a></li>
