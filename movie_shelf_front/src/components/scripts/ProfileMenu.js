d899e79f7aecd25465ce65be1b5d2cad6f282053
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-05-05 19:00:59 -0300

    updated links

--- movie_shelf_front/src/components/scripts/ProfileMenu.js
+++ movie_shelf_front/src/components/scripts/ProfileMenu.js
@@ -4,10 +4,10 @@ function ProfileMenu(){
     return (
     <div className="profile_menu">
         <ul className="profile_menu_options">
-            <li><a href="/MyLists">My lists</a></li>
-            <li><a href="/">Progress</a></li>
-            <li><a href="/">Ratings</a></li>
-            <li><a href="/">Settings</a></li>
+            <li><a href="/lists">My lists</a></li>
+            <li><a href="/progress">Progress</a></li>
+            <li><a href="/ratings">Ratings</a></li>
+            <li><a href="/settings">Settings</a></li>
         </ul>
     </div>
     );
