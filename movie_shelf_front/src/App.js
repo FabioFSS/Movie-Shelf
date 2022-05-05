af1a188b3b66ee424c7728f36d3a46881707e9d3
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-05-04 21:19:34 -0300

    changed main page

--- movie_shelf_front/src/App.js
+++ movie_shelf_front/src/App.js
@@ -1,10 +1,8 @@
-// import Details from './pages/Details/index'
-import Profile from "./pages/Profile/profile";
 import Home from "./pages/Home/index"
 
 function App() {
   return (
-    <Profile />
+    <Home />
   );
 }
 
