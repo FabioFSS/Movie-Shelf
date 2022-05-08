f22fa8bd42f72ccd0b479bd5528818a142581eeb
Welvis Souza <welvisconta@gmaile.com>
2022-05-08 17:14:43 -0300

    add screens settings and ratings

--- movie_shelf_front/src/pages/Settings/index.js
+++ movie_shelf_front/src/pages/Settings/index.js
@@ -13,7 +13,25 @@ export default function Settings() {
                     <Navbar />
                 </Router>
             </div>
-            <h1>Settings Page</h1>
+            <div classsName="container-settings">
+                <h1>Settings</h1>
+                <div>
+                    <img/>
+                    <div>
+                        <h3>Language</h3>
+                        <h3>Theme</h3>
+                        <h3>...</h3>
+                    </div>
+                </div>
+                <div>
+                    <button>
+                        <p>SAVE</p>
+                    </button>
+                    <button>
+                        <p>CANCEL</p>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
