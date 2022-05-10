3de7abfdf4a9ae69f54fb963cc92feb1303c0e83
Welvis Souza <welvisconta@gmaile.com>
2022-05-10 10:45:42 -0300

    aditional configs

--- movie_shelf_front/src/pages/Settings/index.js
+++ movie_shelf_front/src/pages/Settings/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
-import { api, apiKey, language } from "../../services/api";
 import Navbar from '../../components/scripts/Navbar';
 import imgProfile from "../../assets/profile.png"
 import './styles.scss'
