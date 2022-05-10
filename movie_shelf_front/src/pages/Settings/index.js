60648980e325bc9ece5f2f6eba31b7d282aa71f2
Welvis Souza <welvisconta@gmaile.com>
2022-05-10 10:41:56 -0300

    add page settings

--- movie_shelf_front/src/pages/Settings/index.js
+++ movie_shelf_front/src/pages/Settings/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { api, apiKey, language } from "../../services/api";
 import Navbar from '../../components/scripts/Navbar';
+import imgProfile from "../../assets/profile.png"
 import './styles.scss'
 
 export default function Settings() {
@@ -13,22 +14,29 @@ export default function Settings() {
                     <Navbar />
                 </Router>
             </div>
-            <div classsName="container-settings">
-                <h1>Settings</h1>
-                <div>
-                    <img/>
+            <div className="container-settings">
+                <h1 className='neme-screen'>Settings</h1>
+                <div className="options">
+                    <img src={imgProfile} className="img-profile"/>
                     <div>
                         <h3>Language</h3>
+                        <select className="select">
+                            <option value="Select" className='option-select'>pt-BR</option>
+                            <option value="Select" className='option-select'>en-US</option>
+                        </select>
                         <h3>Theme</h3>
-                        <h3>...</h3>
+                        <select className="select">
+                            <option value="Select" className='option-select'>Dark</option>
+                            <option value="Select" className='option-select'>Light</option>
+                        </select>
                     </div>
                 </div>
-                <div>
-                    <button>
-                        <p>SAVE</p>
+                <div className='caintainer-buttons'>
+                    <button className='button'>
+                        <p className='text-button'>SAVE</p>
                     </button>
-                    <button>
-                        <p>CANCEL</p>
+                    <button className='button'>
+                        <p className='text-button'>CANCEL</p>
                     </button>
                 </div>
             </div>
