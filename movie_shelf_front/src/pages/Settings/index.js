576b17d1ea95e4bb80b17ae6c44920708e39b5e2
Welvis Souza <welvisconta@gmaile.com>
2022-05-18 22:45:43 -0300

    update screen settings

--- movie_shelf_front/src/pages/Settings/index.js
+++ movie_shelf_front/src/pages/Settings/index.js
@@ -14,29 +14,67 @@ export default function Settings() {
                 </Router>
             </div>
             <div className="container-settings">
-                <h1 className='neme-screen'>Settings</h1>
-                <div className="options">
-                    <img src={imgProfile} className="img-profile"/>
+
+                <div className='div-transparence'>
+                    <h1 className='neme-screen'>Settings</h1>
+
+                    <div className="options">
+                        <img src={imgProfile} className="img-profile"/>
+                        <div>
+                            <h3>Language</h3>
+                            <select className="select">
+                                <option value="Select" className='option-select'>pt-BR</option>
+                                <option value="Select" className='option-select'>en-US</option>
+                            </select>
+                            <h3>Theme</h3>
+                            <select className="select">
+                                <option value="Select" className='option-select'>Dark</option>
+                                <option value="Select" className='option-select'>Light</option>
+                            </select>
+                        </div>
+                    </div>
+
                     <div>
-                        <h3>Language</h3>
+                        <h3 className="user-name">Name</h3>
+                        <input className="input" type="text" placeholder="User Name"/>
+                    </div>
+
+                    <div>
+                        <h3 className="user-email">Email</h3>
+                        <input className="input" type="text" placeholder="Email@gmailcom"/>
+                    </div>
+
+                    <div className='container-intern-settings'>
+                        <div>
+                            <h3 className="user-age">Age</h3>
+                            <input className="input-age" type="text" placeholder="Age"/>
+                        </div>
+                        <div>
+                            <h3 className="user-gender">Gender</h3>
+                            <select className="select">
+                                <option value="Select" className='option-select'>M</option>
+                                <option value="Select" className='option-select'>W</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="location-container">
+                        <h3 className="user-location">Location</h3>
                         <select className="select">
-                            <option value="Select" className='option-select'>pt-BR</option>
-                            <option value="Select" className='option-select'>en-US</option>
-                        </select>
-                        <h3>Theme</h3>
-                        <select className="select">
-                            <option value="Select" className='option-select'>Dark</option>
-                            <option value="Select" className='option-select'>Light</option>
+                            <option value="Select" className='option-select'>Bahia</option>
+                            <option value="Select" className='option-select'>São Paulo</option>
+                            <option value="Select" className='option-select'>...</option>
                         </select>
                     </div>
-                </div>
-                <div className='caintainer-buttons'>
-                    <button className='button'>
-                        <p className='text-button'>SAVE</p>
-                    </button>
-                    <button className='button'>
-                        <p className='text-button'>CANCEL</p>
-                    </button>
+
+                    <div className='caintainer-buttons'>
+                        <button className='button'>
+                            <p className='text-button'>SAVE</p>
+                        </button>
+                        <button className='button'>
+                            <p className='text-button'>CANCEL</p>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
