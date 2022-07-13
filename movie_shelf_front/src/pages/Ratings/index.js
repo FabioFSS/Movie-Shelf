e78b9a422606f55907dbbc73fc39c87061a8f559
Welvis <welvisconta@gmail.com>
2022-07-13 19:07:12 -0300

    progress add tv shows

--- movie_shelf_front/src/pages/Ratings/index.js
+++ movie_shelf_front/src/pages/Ratings/index.js
@@ -1,6 +1,5 @@
 // react
 import React, { useEffect, useState, useContext } from "react";
-import { FaTrash } from "react-icons/fa";
 
 // other libs
 import axios from "axios";
@@ -11,21 +10,10 @@ import useAxios from "../../utils/useAxios";
 import styles from "./styles.module.css";
 
 // assets
-import capa from "../../assets/image-item/berserk.jpg";
-import profile from "../../assets/profile.png";
 import plus from "../../assets/comments/plus.png";
 import minus from "../../assets/comments/minus.png";
 import sende from "../../assets/comments/sende.png";
 import star from "../../assets/comments/star.png";
-
-const comments = [
-    { comment: "Muito bom!" },
-    { comment: "Não gostei da animação" },
-    { comment: "História muito boa!" },
-    { comment: "Não recomendo." },
-];
-
-
 
 export default function Ratings() {
     const [movieId] = useState(window.location.href.split("=")[1].split("#")[0]);
@@ -64,8 +52,6 @@ export default function Ratings() {
         });
                 
     }
-    
-    console.log(comment);
 
     useEffect(() => {
         const fetchData = async () => {
